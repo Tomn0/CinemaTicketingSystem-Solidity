cf67ed688d543ce485a5d9e2fe9019b2c9f7ae67
Tomn0 <nal.tomek@gmail.com>
2022-11-27 23:29:29 +0100

    updated Cinema.sol

--- Cinema.sol
+++ Cinema.sol
@@ -4,13 +4,21 @@ pragma solidity ^0.8.17;
 
 contract Ticket {
     uint public price;
-    string public filmTitle;
+
     uint public place_id;
-    address public owner;
-    address public ticketingSystem; 
+    address public ticketOwner; 
+    address public ticketingSystem;
+    address public movieAddress;
 
     enum State {Init, Locked, Inactive, Refund}
     State public state;
+
+    constructor(address ticketBuyer, uint256 place, address _movieAddress) {
+        movieAddress = _movieAddress;
+        ticketOwner = ticketBuyer;
+        place_id = place;
+        ticketingSystem = msg.sender;
+    }
 
     ///This function can't be called at this state
     error InvalidState();
@@ -22,12 +30,12 @@ contract Ticket {
         _;
     }
 
-    ///This is not the ticket owner!
-    error notOwner();
+    ///You are not the ticket owner!
+    error notTicketOwner();
 
-    modifier isOwner(address caller){
-        if(caller != owner){
-            revert notOwner();
+    modifier isTicketOwner(address caller){
+        if(caller != ticketOwner){
+            revert notTicketOwner();
         }
         _;
     }
@@ -42,49 +50,128 @@ contract Ticket {
         _;
     }
 
-    constructor(address ticketBuyer, uint256 place) {
-        owner = ticketBuyer;
-        place_id = place;
-        ticketingSystem = msg.sender;
-    }
-
      function getPlaceID() public view returns (uint256) {
         return place_id;
     }
 
-    function changePlace(address caller, uint256 new_place) public isCinema isOwner(caller) {
+    function changePlace(address caller, uint256 new_place) public isCinema isTicketOwner(caller) {
         place_id = new_place;
     }
-
 }
 
 
 contract CinemaTicketingSystem {
-    mapping(address => Ticket) _tickets;
 
+    mapping(address => Ticket[]) _tickets;
+    Movie[] public movies;
+    
     uint public price;
     uint public qty;
     address payable public buyer;
-    address payable public seller;
+    address payable public owner;
 
-
-    function buyTicket(uint256 place) public {
-        require (_tickets[msg.sender] == Ticket(address(0)));
-        _tickets[msg.sender] = new Ticket(msg.sender, place);
+    constructor(){
+        owner = payable(msg.sender);
     }
 
-    function changePlace(uint new_place) public {
-        require (_tickets[msg.sender] != Ticket(address(0)));
-        Ticket(_tickets[msg.sender]).changePlace(msg.sender, new_place);
+    ///You are not owner of this cinema!
+    error notOwner();
+
+    modifier isOwner(){
+        if(msg.sender != owner){
+            revert notOwner();
+        }
+        _;
     }
 
-    function getPlace(address account) public view returns (uint256) {
-        require (_tickets[account] != Ticket(address(0)));
-        return (_tickets[account].getPlaceID());
+    function getAllInformation(Ticket ticket) public view returns( address owner,
+        uint place,
+        address movieAddress){
+        return (ticket.ticketOwner(),ticket.place_id(),ticket.movieAddress());
     }
 
-    function getMyPlace() public view returns (uint256) {
-        return (getPlace(msg.sender));
+    function printAllInformations(ticketInformation memory ticketInfo) public view returns(address owner,
+        uint place,
+        address movieAddress){
+        return (ticketInfo.owner, ticketInfo.place, ticketInfo.movieAddress);
     }
 
+    function getAllMovies() public view returns (Movie[] memory) {
+        return movies;
+    }
+
+    function getTickets(address client) public view returns(Ticket[] memory){
+        return _tickets[client];
+    }
+
+    function buyTicket(uint256 place, address movieAddress) public payable {
+        // require (_tickets[msg.sender] == Ticket(0));
+        require(msg.value == movies[0].price(), "Insufficient Balance");
+        owner.transfer(msg.value);
+        _tickets[msg.sender].push(new Ticket(msg.sender, place, movieAddress));
+        movies[0].reserveSeat(place);
+    }
+
+    // function changePlace(uint new_place) public {
+    //     // require (_tickets[msg.sender] != Ticket(0));
+    //     Ticket(_tickets[msg.sender]).changePlace(msg.sender, new_place);
+    // }
+
+    // function getPlace(address account) public view returns (uint256) {
+    //     // require (_tickets[account] != Ticket(0));
+    //     return (_tickets[account].getPlaceID());
+    // }
+
+    // function getMyPlace() public view returns (uint256) {
+    //     return (getPlace(msg.sender));
+    // }
+
+    function addMovie(string memory title, string memory date, uint placesCount, uint hall, string memory cinemaAddress, uint price) isOwner public  {
+        movies.push(new Movie(title, date, placesCount, hall, cinemaAddress, price));
+    }
+
+    function checkSeat(uint placeNumber, address movieAddress) public view returns(bool){
+        return movies[0].isSeatFree(placeNumber);
+    }
+}
+
+
+contract Movie {
+    string public title;
+    string public date;
+    uint public price;
+    uint public hall;
+    uint public placesCount;
+    string cinemaAddress;
+    mapping(uint => bool) isReserved;
+
+    constructor(string memory _title, string memory _date, uint _placesCount, uint _hall, string memory _cinemaAddress, uint _price) {
+        title = _title;
+        date = _date;
+        hall = _hall;
+        price = _price;
+        placesCount = _placesCount;
+        cinemaAddress = _cinemaAddress;
+        for(uint i;i<_placesCount;i++){
+            isReserved[i] = false;
+        }
+    }
+
+    function isSeatFree(uint placeNumber) public view returns(bool){
+        return isReserved[placeNumber];
+    }
+
+    ///This place is reserved by another person or place number is not valid
+    error notFree();
+
+    modifier isFree(uint seatNumber){
+        if(isReserved[seatNumber] == true && seatNumber <= placesCount && seatNumber > 0){
+            revert notFree();
+        }
+        _;
+    }
+
+    function reserveSeat(uint seatNumber) isFree(seatNumber) public{
+        isReserved[seatNumber] = true;
+    }
 }
